0d32fb34fe4801c35f5ac8c76b3fc241d3b78551
pope <lx1264357963@163.com>
2020-03-04 15:20:33 +0800

    修改主页轮播图信息为从后端获取

--- js/main.js
+++ js/main.js
@@ -1,3 +1,76 @@
+//轮播图图片
+var carouselImages = [
+	{
+		url: "../static/book-2391006_1920.jpg",
+		labels: ["图片一", "测试", "图片"],
+		description: "This is the first book."
+	},
+	{
+		url: "../static/book-3692762_1920.jpg",
+		labels: ["图片二", "测试", "图片"],
+		description: "This is the second book."
+	},
+	{
+		url: "../static/book-2435578_1920.jpg",
+		labels: ["图片三", "测试", "图片"],
+		description: "This is the third book."
+	}
+];
+//获取轮播图图片
+
+//设置html轮播图
+var carouselInner = document.getElementById("carouselInner");
+var carouselInnerSrc = "";
+for (let i = 0; i < carouselImages.length; i++) {
+	if (i === 0) {
+		carouselInnerSrc +=
+			"<div class = 'carousel-item active'>" +
+			"<img src='" + carouselImages[i].url + "' class = 'img-fluid mw-100 h-auto' alt='book image'>" +
+			"</div>"
+	} else {
+		carouselInnerSrc +=
+			"<div class = 'carousel-item'>" +
+			"<img src='" + carouselImages[i].url + "' class = 'img-fluid mw-100 h-auto' alt='book image'>" +
+			"</div>"
+	}
+}
+carouselInner.innerHTML = carouselInnerSrc;
+
+
+{
+	//设置书籍图片
+	let bookImage = document.getElementById("bookImage");
+	bookImage.src = carouselImages[0].url;
+	//设置书籍标签
+	let bookLabels = document.getElementById("bookLabels");
+	let labels = "";
+	for (let i = 0; i < carouselImages[0].labels.length; i++) {
+		labels += "<p class='badge badge-secondary mx-2 my-1'>" + carouselImages[0].labels[i] + "</p>";
+	}
+	bookLabels.innerHTML = labels;
+	//设置描述
+	let bookDescription = document.getElementById("bookDescription");
+	bookDescription.innerText = carouselImages[0].description;
+}
+
+//当轮播图切换时调用该事件
+$('#carouselExampleIndicators').on('slide.bs.carousel', function (event) {
+	//设置书籍图片
+	let bookImage = document.getElementById("bookImage");
+	bookImage.src = carouselImages[event.to].url;
+	//设置书籍标签
+	let bookLabels = document.getElementById("bookLabels");
+	let labels = "";
+	for (let i = 0; i < carouselImages[event.to].labels.length; i++) {
+		labels += "<p class='badge badge-secondary mx-2 my-1'>" + carouselImages[event.to].labels[i] + "</p>";
+	}
+	bookLabels.innerHTML = labels;
+	//设置描述
+	let bookDescription = document.getElementById("bookDescription");
+	bookDescription.innerText = carouselImages[event.to].description;
+});
+
+
 function previous() {
 
 }
@@ -16,19 +89,13 @@ function next() {
 
 }
 
-function next_selected(){
+function next_selected() {
 	let next = document.getElementById("next");
 	next.src = "../static/next_selected.png";
 }
 
-function next_unselected(){
+function next_unselected() {
 	let next = document.getElementById("next");
 	next.src = "../static/next.png";
 }
 
-//当轮播图切换时调用该事件
-$('#carouselExampleIndicators').on('slide.bs.carousel', function (event) {
-
-	//输出当前图片索引
-	console.log(event.from)
-});
